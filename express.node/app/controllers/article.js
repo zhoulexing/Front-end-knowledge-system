3f2d18086d9dbf35408d284e1409ffa1fdc8ac48
zhoulexing <2831518478@qq.com>
2018-05-16 17:09:34 +0800

    add middle-dispatch

--- express.node/app/controllers/article.js
+++ express.node/app/controllers/article.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models');
+const db = require('../../utils/mysql.jdbc');
 
 router.get('/', (req, res, next) => {
   db.Article.findAll().then((articles) => {
--- express.node/utils/sync.writing.js
+++ express.node/utils/sync.writing.js
@@ -15,50 +15,52 @@ function getValue(value) {
   });
 }
 
-const gen = test();
+//const gen = test();
 
 
 
-gen.next().value.then(value => {
-});
-gen.next().value.then(value => {
-});
+// gen.next().value.then(value => {
+// });
+// gen.next().value.then(value => {
+// });
 
 
-co(function* test2() {
+const val2 = co(function* test2() {
   const val1 = yield getValue("hello");
   const val2 = yield getValue("world");
+  return val1 + " " + val2;
 });
-
-
-async function testAsync() {
-  const value = await getValue("test async");
-  return value;
-}
-const value4 = testAsync();
-
-
-
-Promise.all([
-  getValue('a'),
-  getValue('b'),
-  new Promise((resolve, reject) => { reject('c') }),
-  Promise.resolve('d')
-]).then((value) => {
-  console.log(value);
-}, (value) => {
-  console.log(value);
+val2.then(val2 => {
+  console.log(val2);
 });
+// async function testAsync() {
+//   const value = await getValue("test async");
+//   return value;
+// }
+// const value4 = testAsync();
 
-let isTrue = true;
-let aaa;
-if(isTrue) {
-  aaa = getValue('a');
-} else {
-  aaa = getValue('b');
-}
-aaa.then(value => {
-  console.log(value)
-});
+
+
+// Promise.all([
+//   getValue('a'),
+//   getValue('b'),
+//   new Promise((resolve, reject) => { reject('c') }),
+//   Promise.resolve('d')
+// ]).then((value) => {
+//   console.log(value);
+// }, (value) => {
+//   console.log(value);
+// });
+
+// let isTrue = true;
+// let aaa;
+// if(isTrue) {
+//   aaa = getValue('a');
+// } else {
+//   aaa = getValue('b');
+// }
+// aaa.then(value => {
+//   console.log(value)
+// });
 
 
