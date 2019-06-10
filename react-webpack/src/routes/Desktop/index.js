6560a88fd03ce0b4e0fcf06ae6c4be7a1e8a88c9
zhoulexing <2831518478@qq.com>
2019-06-10 23:21:58 +0800

    test getDerivedStateFromProps

--- react-webpack/src/routes/Desktop/index.js
+++ react-webpack/src/routes/Desktop/index.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Button } from "antd";
+import Count from "./count";
 
 class Desktop extends Component {
+
+    constructor(props) {
+        super(props);
+        props.dispatch({
+            type: "global/add",
+            payload: {
+                count: 1,
+            }
+        });
+    }
+
+    componentDidMount() {
+        console.log(this.props.count);
+    }
+
     render() {
         return (
             <div>
                 <Button size="large" type="primary" onClick={this.add}>Desktop</Button>
+                <Count />
             </div>
         )
     }
@@ -21,4 +38,10 @@ class Desktop extends Component {
     }
 }
 
-export default connect()(Desktop);
+function mapStateToProps({ global }) {
+    return {
+        count: global.count,
+    }
+}
+
+export default connect(mapStateToProps)(Desktop);
