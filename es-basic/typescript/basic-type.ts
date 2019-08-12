dccf8ac61dbfc42a92a4fb8f3ff1f67ce4014943
800BEST\BG385352 <yxzhou@best-inc.com>
2019-08-12 20:51:12 +0800

    ...

--- es-basic/typescript/basic-type.ts
+++ es-basic/typescript/basic-type.ts
@@ -3,7 +3,7 @@ let num: number = 1;
 let arr: number[] = [1,2,3];
 let arr2: Array<number> = [1,2,3];
 let tuple: [string, number] = ["", 1];
-enum Color { Red = "hello", Green, Blue };
+enum Color { Red, Green, Blue };
 let c: Color = Color.Green;
 let notTrue: any = 4;
 let unusable: void = undefined;
