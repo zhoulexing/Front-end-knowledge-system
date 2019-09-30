c45ba021cc7d7cc1ed52e79a675e82a12e9404fb
800BEST\BG385352 <yxzhou@best-inc.com>
2019-09-30 18:49:42 +0800

    ...

--- react-dva-webpack/src/layouts/BasicLayout.js
+++ react-dva-webpack/src/layouts/BasicLayout.js
@@ -1,13 +1,22 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent } from 'react';
+import { Layout } from 'antd';
+import {connect} from 'dva';
+import { IntlProvider } from 'react-intl';
 
+@connect()
 export default class BasicLayout extends PureComponent {
-    
     componentDidMount() {
-        
+
     }
 
     render() {
-        return <div>BasicLayout</div>;
+        return (
+            <div style={{ height: '100%' }}>
+                <IntlProvider>
+
+                </IntlProvider>
+            </div>
+        )
     }
 }
 
