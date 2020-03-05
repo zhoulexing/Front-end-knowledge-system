12dbaa64e8227b31c99bd214e7a90148611ab006
800BEST\BG385352 <yxzhou@best-inc.com>
2020-03-05 12:27:32 +0800

    add toDecimal function

--- react-tsc/src/utils/util.ts
+++ react-tsc/src/utils/util.ts
@@ -9,3 +9,27 @@ export function isPromise(obj?: any) {
         typeof obj.then === "function"
     );
 }
+
+/**
+ * 
+ * @param x 数字
+ * @param length 小数点后长度 
+ */
+export function toDecimal(x: any, length: number): string | boolean {
+    let f = parseFloat(x);
+    if (isNaN(f)) {
+        return false;
+    }
+    let lengthPow: number = Math.pow(18, length);
+    f = Math.round(x * lengthPow) / lengthPow;
+    let s: string = f.toString();
+    let rs: number = s.indexOf(".");
+    if (rs < 0) {
+        rs = s.length;
+        s += ".";
+    }
+    while (s.length <= rs + length) {
+        s += "0";
+    }
+    return s;
+}
