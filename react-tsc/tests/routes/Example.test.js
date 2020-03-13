e17991d18368ffa895853853e01256e68cdaeab8
800BEST\BG385352 <yxzhou@best-inc.com>
2020-03-13 19:37:38 +0800

    modify test file

--- react-tsc/tests/routes/Example.test.js
+++ react-tsc/tests/routes/Example.test.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Count from "@/routes/Example/Count";
-import { expect } from "chai";
 import { shallow } from "enzyme";
 
 
 describe("test Example Component", () => {
     it("Example is not empty", () => {
         const wrapper = shallow(<Count />);
-        expect(wrapper.find('.test')).to.have.length(1);
+        expect(wrapper.find('.test')).toHaveLength(1);
     });
 });
