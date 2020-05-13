34aa817860707ac857e8dc54f3ca7b63a23dc778
zhoulexing <2831518478@qq.com>
2020-05-13 23:06:40 +0800

    modify typescript

--- basic/typescript/src/class.ts
+++ basic/typescript/src/class.ts
@@ -1,112 +1,41 @@
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
-}
-let greeter = new Greeter("world");
-
-
-class Animal {
-    private age: number;
-    name: string;
-    constructor(theName: string) {
-        this.name = theName;
-        this.age = 25;
-    }
-    move(distanceInMeters: number = 0) {
-        console.log(`${this.name} moved ${distanceInMeters}m.`);
-    }
-}
-class Snake extends Animal {
-    constructor(name: string) {
-        super(name);
-    }
-
-    move(distanceInMeters = 5) {
-        console.log("Slithering...");
-        super.move(distanceInMeters);
-    }
-}
-class Horse extends Animal {
-    constructor(name: string) {
-        super(name);
-    }
-
-    move(distanceInMeters = 45) {
-        console.log("Galloping...");
-        super.move(distanceInMeters);
-    }
-}
-let sam = new Snake("Sammy the Python");
-let tom: Animal = new Horse("Tommy the Palomino");
-sam.move();
-tom.move(34);
-
-class Employee {
-    private _fullName: string;
-    constructor() {
-        this._fullName = "";
-    }
-
-
-    get fullName(): string {
-        return this._fullName;
-    }
-
-    set fullName(fullName: string) {
-        this._fullName = fullName;
-    }
-}
-
-
-class Queue {
-    private data = [];
-    push = () => {
-        
-    }
-}
-
-abstract class Animal1 {
-    abstract makeSound(): void;
-    move(): void {
-        console.log('Hello World!');
-    }
-}
-
-class Cat extends Animal1 {
-    makeSound() {
-        console.log("miao miao");
-    }
-}
-
-const cat = new Cat();
-cat.move();
-
-class Car {
-    public run() {
-
-    }
-    protected run1() {
-
-    }
-    private run2() {
-
-    }
-}
-
-const car = new Car();
-car.run();
-
-
 function logParameter(target: object, propertyKey: string, index: number) {
     console.log(target, propertyKey, index);
 }
-class Person {
-    greet(@logParameter message: string, @logParameter name: string) {
+
+class MyClass {
+    str: string;
+    public str1?: string;
+    private str2?: string;
+    protected str3?: string;
+    constructor(str: string) {
+        this.str = str;
+    }
+    get _str(): string {
+        return this.str;
+    }
+    set _str(str: string) {
+        this.str = str;
+    }
+    public func() {
+        console.log("func");
+    }
+    private func2() {}
+    protected func3() {}
+    func4(@logParameter message: string, @logParameter name: string) {
         return `${message} ${name}`;
     }
 }
+
+abstract class MyClass1 {
+    abstract func(): void;
+    func1(): void {
+        console.log('func1');
+    }
+}
+
+class Myclass2 extends MyClass1 {
+    func() {
+        console.log("func");
+    }
+}
+
--- basic/typescript/src/interface.ts
+++ basic/typescript/src/interface.ts
@@ -1,141 +1,39 @@
-/* 初探 */
-interface LabelledValue {
-    label: string;
-    color?: string;
-    readonly name: string;
+interface Fun1 {
+    (word: string): string;
 }
-function printLabel(labelObj: LabelledValue) {
-    // labelObj.name = "yww"; error
-    console.log(labelObj.label);
-}
-let myObj = {
-    size: 10, 
-    label: "Size 10 Object",
-    name: "zlx",
-};
-printLabel(myObj);
 
-/* 只读 */
-let a: number[] = [1,2,3,4];
-let ro: ReadonlyArray<number> = a;
-a.push(5);
-// ro.push(5); error
-
-/* 函数类型 */
-interface SearchFunc {
-    (source: string, subString: string): boolean;
-}
-let mySearch: SearchFunc;
-mySearch = function(src, sub) {
-    let result = src.search(sub);
-    return result > 1;
-};
-
-/* 可索引的类型 */
-interface StringArray {
+interface Arr2 {
     [index: number]: string;
 }
-let myArray: StringArray = ["Bob", "Fred"];
-let myStr: string = myArray[0];
 
-
-/* 类类型 */
-interface ClockInterface {
-    currentTime: Date;
-    setTime(d: Date): void;
-}
-class Clock implements ClockInterface {
-    currentTime: Date;
-    constructor(h: number, m: number) {
-        this.currentTime = new Date();
-    }
-    setTime(d: Date): void {
-        this.currentTime = d;
-    }
+interface Obj {
+    [propName: string]: any;
 }
 
-interface Shape {
-    color: string;
-}
-interface PenStroke {
-    penWidth: number;
-}
-interface Square extends Shape, PenStroke {
-    sideLength: number;
-}
-let square = <Square>{};
-square.color = "red";
-square.sideLength = 5;
-square.penWidth = 4;
-
-
-interface MenuItem {
-    name: string,
-    icon: string,
-    path: string,
-    children: MenuItem[]
+interface Obj1 {
+    [name: string]: string;
 }
 
-interface MenuData extends Array<MenuItem> {}
-
-// interface MenuData {
-//     [index: number]: MenuItem
-// }
-
-// type MenuData = MenuItem[];
-
-const menuData = [
-    {
-        name: "数据资源",
-        icon: "database",
-        path: "datasource"
-    },
-    {
-        name: "我的",
-        icon: "user",
-        path: "my",
-        children: [
-            {
-                name: "研判",
-                path: "determine"
-            },
-            {
-                name: "资源",
-                path: "datasource"
-            }
-        ]
-    },
-    {
-        name: "示例",
-        icon: "smile",
-        path: "example",
-        children: [
-            {
-                name: "示例1",
-                path: "1"
-            },
-            {
-                name: "示例2",
-                path: "2"
-            }
-        ]
-    }
-];
-
-interface Config {
-    width?: string;
-    [propName:string]: any;
+interface Inter {
+    num: number;
+    str: string;
+    str1?: string; // 可选属性
+    readonly str2: string; // 只度属性
+    fun: (word: string) => string; // 函数类型
+    fun1: Fun1; 
+    fun2: (word: string) => void;
+    func3: (date: Date) => Date;
+    arr: string[];
+    arr1: Array<string>;
+    arr2: Arr2;
+    obj: Obj;
+    obj1: Obj1;
+    children: Inter;
 }
 
-interface Config1 {
-    [name:string]: string
-}
+const inter = {} as Inter;
+inter.fun2 = (str) => str;
+inter.fun2("hello");
 
-interface Person {
-    name: string;
-    age: number;
-}
-
-const person = {} as Person;
-person.name = "zlx";
-person.age = 20;
+const inter1 = <Inter>{};
+inter1.num = 1;
