0daefe858c99d56dd135552e29c7803d2eb54e1e
800BEST\BG385352 <yxzhou@best-inc.com>
2020-06-18 19:30:47 +0800

    add nest

--- node-server/nest/src/main.ts
+++ node-server/nest/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ApplicationModule } from './app.module';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3002);
+    const appOptions = { cors: true };
+    const app = await NestFactory.create(ApplicationModule, appOptions);
+    app.setGlobalPrefix('api');
+
+    // const options = new DocumentBuilder()
+    //     .setTitle('NestJS Realworld Example App')
+    //     .setDescription('The Realworld API description')
+    //     .setVersion('1.0')
+    //     .setBasePath('api')
+    //     .addBearerAuth()
+    //     .build();
+    // const document = SwaggerModule.createDocument(app, options);
+    // SwaggerModule.setup('/docs', app, document);
+
+    await app.listen(3002);
 }
 bootstrap();
