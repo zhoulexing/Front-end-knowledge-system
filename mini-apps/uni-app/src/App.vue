fe366d0a2e1d07a952995cec331f338765ee721d
800BEST\BG385352 <yxzhou@best-inc.com>
2020-07-15 18:09:54 +0800

    add uni-app

---
+++ mini-apps/uni-app/src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export default {
+        onLaunch() {
+            console.log('App Launch');
+        },
+        onShow() {
+            console.log('App Show');
+        },
+        onHide() {
+            console.log('App Hide');
+        },
+        globalData: {
+            userInfo: null
+        }
+    };
+</script>
+
+<style>
+    /*每个页面公共css */
+</style>
