6a600b66f4f8b2e38a586877d6d297083cbecf89
800BEST\BG385352 <yxzhou@best-inc.com>
2020-11-19 17:57:43 +0800

    ...

--- frame-frontends/react-app/config-overrides.js
+++ frame-frontends/react-app/config-overrides.js
@@ -4,29 +4,42 @@ const {
     addLessLoader,
     addDecoratorsLegacy,
     addPostcssPlugins,
-    disableEsLint
-} = require('customize-cra');
+    disableEsLint,
+    overrideDevServer,
+} = require("customize-cra");
 
-module.exports = override(
-    addDecoratorsLegacy(),
-    fixBabelImports("import", {
-        libraryName: 'antd-mobile',
-        style: true
-    }),
-    addLessLoader({
-        lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: {
-                'background-color': '#fbfbfb',
-                'radius-md': '2px'
-            }
-        }
-    }),
-    addPostcssPlugins(
-        [require('postcss-pxtorem')({
-            rootValue: 16,
-            propList: ['*']
-        })]
+const addCustomize = () => config => {
+    if(process.env.NODE_ENV === 'production') {
+        config.output.publicPath = './demo';
+        return config;
+    }
+}
+
+
+module.exports = {
+    webpack: override(
+        addDecoratorsLegacy(),
+        fixBabelImports("import", {
+            libraryName: "antd-mobile",
+            style: true,
+        }),
+        addLessLoader({
+            lessOptions: {
+                javascriptEnabled: true,
+                modifyVars: {
+                    "background-color": "#fbfbfb",
+                    "radius-md": "2px",
+                },
+            },
+        }),
+        addPostcssPlugins([
+            require("postcss-pxtorem")({
+                rootValue: 16,
+                propList: ["*"],
+            }),
+        ]),
+        disableEsLint(),
+        addCustomize()
     ),
-    disableEsLint()
-);
+    devServer: overrideDevServer(devServerConfig()),
+};
