6c3d239f840f7b88d599f8b03bae3d487e127534
zhoulexing <2831518478@qq.com>
2021-01-04 22:44:44 +0800

    修改 taro

--- basic/taro/taro-helper/npm.js
+++ basic/taro/taro-helper/npm.js
@@ -1,37 +1,164 @@
-import fs from "fs-extra";
-import path from "path";
-import { SCRIPT_EXT } from "./constants";
+import resolvePath from 'resolve'
+import spawn from 'cross-spawn'
+import chalk from 'chalk'
 
-export function resolveScriptPath(p) {
-    return resolveMainFilePath(p);
+import * as Util from './utils'
+
+const PEERS = /UNMET PEER DEPENDENCY ([a-z\-0-9.]+)@(.+)/gm
+const npmCached = {}
+
+const erroneous = []
+
+const defaultInstallOptions = {
+    dev: false,
+    peerDependencies: true
 }
 
-export function resolveMainFilePath(p, extArrs = SCRIPT_EXT) {
-    const realPath = p;
-    const taroEnv = process.env.TARO_ENV;
-    for (let i = 0; i < extArrs.length; i++) {
-        const item = extArrs[i];
-        if (taroEnv) {
-            if (fs.existsSync(`${p}.${taroEnv}${item}`)) {
-                return `${p}.${taroEnv}${item}`;
-            }
-            if (fs.existsSync(`${p}${path.sep}index.${taroEnv}${item}`)) {
-                return `${p}${path.sep}index.${taroEnv}${item}`;
-            }
-            if (
-                fs.existsSync(
-                    `${p.replace(/\/index$/, `.${taroEnv}/index`)}${item}`
-                )
-            ) {
-                return `${p.replace(/\/index$/, `.${taroEnv}/index`)}${item}`;
-            }
+export const taroPluginPrefix = '@tarojs/plugin-'
+
+export function resolveNpm(pluginName, root) {
+    if (!npmCached[pluginName]) {
+        return new Promise((resolve, reject) => {
+            resolvePath(`${pluginName}`, {
+                basedir: root
+            }, (err, res) => {
+                if (err) {
+                    return reject(err)
+                }
+                npmCached[pluginName] = res
+                resolve(res)
+            })
+        })
+    }
+    return Promise.resolve(npmCached[pluginName])
+}
+
+export function resolveNpmSync(pluginName, root) {
+    try {
+        if (!npmCached[pluginName]) {
+            const res = resolvePath.sync(pluginName, {
+                basedir: root
+            })
+            return res
         }
-        if (fs.existsSync(`${p}${item}`)) {
-            return `${p}${item}`;
+        return npmCached[pluginName]
+    } catch (err) {
+        if (err.code === 'MODULE_NOT_FOUND') {
+            console.log(chalk.cyan(`缺少npm包${pluginName}，开始安装...`))
+            const installOptions = {
+                dev: false
+            }
+            if (pluginName.indexOf(taroPluginPrefix) >= 0) {
+                installOptions.dev = true
+            }
+            installNpmPkg(pluginName, installOptions)
+            return resolveNpmSync(pluginName, root)
         }
-        if (fs.existsSync(`${p}${path.sep}index${item}`)) {
-            return `${p}${path.sep}index${item}`;
+        return ''
+    }
+}
+
+export function installNpmPkg(pkgList, options) {
+    if (!pkgList) {
+        return
+    }
+    if (!Array.isArray(pkgList)) {
+        pkgList = [pkgList]
+    }
+    pkgList = pkgList.filter(dep => {
+        return erroneous.indexOf(dep) === -1
+    })
+
+    if (!pkgList.length) {
+        return
+    }
+    options = Object.assign({}, defaultInstallOptions, options)
+    let installer = ''
+    let args = []
+
+    if (Util.shouldUseYarn()) {
+        installer = 'yarn'
+    } else if (Util.shouldUseCnpm()) {
+        installer = 'cnpm'
+    } else {
+        installer = 'npm'
+    }
+
+    if (Util.shouldUseYarn()) {
+        args = ['add'].concat(pkgList).filter(Boolean)
+        args.push('--silent', '--no-progress')
+        if (options.dev) {
+            args.push('-D')
+        }
+    } else {
+        args = ['install'].concat(pkgList).filter(Boolean)
+        args.push('--silent', '--no-progress')
+        if (options.dev) {
+            args.push('--save-dev')
+        } else {
+            args.push('--save')
         }
     }
-    return realPath;
+    const output = spawn.sync(installer, args, {
+        stdio: ['ignore', 'pipe', 'inherit']
+    })
+    if (output.status) {
+        pkgList.forEach(dep => {
+            erroneous.push(dep)
+        })
+    }
+    let matches
+    const peers = []
+
+    while ((matches = PEERS.exec(output.stdout))) {
+        const pkg = matches[1]
+        const version = matches[2]
+        if (version.match(' ')) {
+            peers.push(pkg)
+        } else {
+            peers.push(`${pkg}@${version}`)
+        }
+    }
+    if (options.peerDependencies && peers.length) {
+        console.info('正在安装 peerDependencies...')
+        installNpmPkg(peers, options)
+    }
+    return output
+}
+
+export const callPlugin = async (pluginName, content, file, config, root) => {
+    const pluginFn = await getNpmPkg(`${taroPluginPrefix}${pluginName}`, root)
+    return pluginFn(content, file, config)
+}
+
+export const callPluginSync = (pluginName, content, file, config, root) => {
+    const pluginFn = getNpmPkgSync(`${taroPluginPrefix}${pluginName}`, root)
+    return pluginFn(content, file, config)
+}
+
+export function getNpmPkgSync(npmName, root) {
+    const npmPath = resolveNpmSync(npmName, root)
+    const npmFn = require(npmPath)
+    return npmFn
+}
+
+export async function getNpmPkg(npmName, root) {
+    let npmPath
+    try {
+        npmPath = resolveNpmSync(npmName, root)
+    } catch (err) {
+        if (err.code === 'MODULE_NOT_FOUND') {
+            console.log(chalk.cyan(`缺少npm包${npmName}，开始安装...`))
+            const installOptions = {
+                dev: false
+            }
+            if (npmName.indexOf(taroPluginPrefix) >= 0) {
+                installOptions.dev = true
+            }
+            installNpmPkg(npmName, installOptions)
+            npmPath = await resolveNpm(npmName, root)
+        }
+    }
+    const npmFn = require(npmPath)
+    return npmFn
 }
--- basic/taro/taro-helper/utils.js
+++ basic/taro/taro-helper/utils.js
@@ -2,8 +2,15 @@ import path from "path";
 import findWorkspaceRoot from "find-yarn-workspace-root";
 import fs from "fs-extra";
 import {
-    processTypeEnum
+    processTypeEnum,
+    SCRIPT_EXT
 } from "./constants";
+import {
+    func
+} from '@hapi/joi';
+import {
+    remove
+} from 'lodash';
 
 export const getModuleDefaultExport = (exports) =>
     exports.__esModule ? exports.default : exports;
@@ -44,4 +51,64 @@ export function addPlatforms(platform) {
     const upperPlatform = platform.toLocaleUpperCase();
     if (PLATFORMS[upperPlatform]) return;
     PLATFORMS[upperPlatform] = platform;
+}
+
+const retries = (process.platform === 'win32') ? 100 : 1;
+export function emptyDirectory(dirPath, opts) {
+    if (fs.existsSync(dirPath)) {
+        fs.readdirSync(dirPath).forEach(file => {
+            const curPath = path.join(dirPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                let removed = false;
+                let i = 0;
+                do {
+                    try {
+                        if (!opts.excludes.length || !opts.excludes.some(item => curPath.indexOf(item) >= 0)) {
+                            emptyDirectory(curPath);
+                            fs.rmdirSync(curPath);
+                        }
+                        removed = true;
+                    } catch (error) {
+
+                    } finally {
+                        if (++i < retries) {
+                            continue;
+                        }
+                    }
+                } while (!removed)
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+    }
+}
+
+export function resolveScriptPath(p) {
+    return resolveMainFilePath(p)
+}
+
+export function resolveMainFilePath(p, extArrs = SCRIPT_EXT) {
+    const realPath = p
+    const taroEnv = process.env.TARO_ENV
+    for (let i = 0; i < extArrs.length; i++) {
+        const item = extArrs[i]
+        if (taroEnv) {
+            if (fs.existsSync(`${p}.${taroEnv}${item}`)) {
+                return `${p}.${taroEnv}${item}`
+            }
+            if (fs.existsSync(`${p}${path.sep}index.${taroEnv}${item}`)) {
+                return `${p}${path.sep}index.${taroEnv}${item}`
+            }
+            if (fs.existsSync(`${p.replace(/\/index$/, `.${taroEnv}/index`)}${item}`)) {
+                return `${p.replace(/\/index$/, `.${taroEnv}/index`)}${item}`
+            }
+        }
+        if (fs.existsSync(`${p}${item}`)) {
+            return `${p}${item}`
+        }
+        if (fs.existsSync(`${p}${path.sep}index${item}`)) {
+            return `${p}${path.sep}index${item}`
+        }
+    }
+    return realPath
 }
